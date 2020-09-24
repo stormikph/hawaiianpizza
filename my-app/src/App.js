3583ea598d647e05e3be8c7aaaa2ea2fd8011747
stormi <stormikhashimoto@gmail.com>
2020-09-23 23:41:11 -0700

    save user time input

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-	console.log(new Date().toLocaleString());
-	return (
-		<div className='App'>
-			<Header />
-			<TimeInputContainer />
-			<TimeDisplay />
-		</div>
-	);
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			time: 0,
+		};
+		this.handleEnterTime = this.handleEnterTime.bind(this);
+	}
+
+	handleEnterTime(timeInputValue) {
+		//e.target.value is always a string
+		const time = Number(timeInputValue);
+		if (time !== 'number' && Number.isNaN(time)) return;
+		this.setState({
+			...this.state,
+			time,
+		});
+	}
+
+	render() {
+		return (
+			<div className='App'>
+				<Header />
+				<TimeInputContainer handleEnterTime={this.handleEnterTime} />
+				<TimeDisplay />
+			</div>
+		);
+	}
 }
 
 export default App;
@@ -19,16 +38,52 @@ function Header() {
 }
 
 class TimeInputContainer extends Component {
+	constructor() {
+		super();
+		this.state = {
+			timeInputValue: '',
+		};
+		this.handleTimeChange = this.handleTimeChange.bind(this);
+		this.handleSubmitTime = this.handleSubmitTime.bind(this);
+	}
+
+	handleTimeChange(e) {
+		const userInput = e.target.value;
+
+		this.setState({
+			...this.state,
+			timeInputValue: userInput,
+		});
+	}
+
+	handleSubmitTime() {
+		const { handleEnterTime } = this.props;
+		const { timeInputValue } = this.state;
+
+		handleEnterTime(timeInputValue);
+		this.setState({
+			...this.state,
+			timeInputValue: '',
+		});
+	}
+
 	render() {
+		const { timeInputValue } = this.state;
+
 		return (
 			<div className='time-input-container'>
-				<input type='text' />
-				<button>Enter</button>
+				<input
+					type='text'
+					placeholder='Enter time in minutes'
+					value={timeInputValue}
+					onChange={this.handleTimeChange}
+				/>
+				<button onClick={this.handleSubmitTime}>Enter</button>
 			</div>
 		);
 	}
 }
 
 function TimeDisplay() {
-	return <p>hit</p>;
+	return <p>{new Date().toLocaleDateString()}</p>;
 }
